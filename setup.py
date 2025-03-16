ed487f58920bcb60b5a81bbc2d39cc1caf26509c
Dipesh Lohchab <dipeshlohchab0302@gmail.com>
2025-03-16 22:59:59 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Custumor segmentation",
+    version="0.0.1",
+    author="Dipesh Lohchab",
+    author_email="dipeshlohchab0302@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
